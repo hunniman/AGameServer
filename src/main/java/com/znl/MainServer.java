7a817ca9f047d73745ba5cc8bc743590e96740f6
pengweiyuan <pengweiyuang@gmail.com>
2016-05-29 12:23:22 +0800

    Initial commit

---
+++ src/main/java/com/znl/MainServer.java
@@ -0,0 +1,9 @@
+package com.znl;
+
+/**
+ * Created by pengweiyuan on 5/27/16.
+ */
+public class MainServer {
+
+
+}
---
+++ src/main/java/com/znl/net/NettyServer.java
@@ -0,0 +1,8 @@
+package com.znl.net;
+
+/**
+ * Created by pengweiyuan on 5/28/16.
+ */
+public class NettyServer {
+
+}
